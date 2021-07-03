9554184a3c0e7441768129ce2b3fd5bb5c5036f1
atiqul <md_atiqulhaque@yahoo.com>
2021-07-03 18:24:34 +0600

    add environment module

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 // app object - module scaffolding
 const app = {};
@@ -21,8 +22,8 @@ app.config = {
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
 
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
